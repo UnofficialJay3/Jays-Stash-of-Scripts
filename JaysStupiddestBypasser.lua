8af2fdb7f3bc27f5998454a027adb548d34ac6e6
Jayyyyyyyyyyyyyyyyyyyyyyyyy <KhangJayden2013@gmail.com>
2025-09-13 22:17:39 -0700

    Update JaysStupiddestBypasser.lua

--- JaysStupiddestBypasser.lua
+++ JaysStupiddestBypasser.lua
@@ -1,83 +1,20 @@
--- Scripted by @IlIl_ILovAltAccsHAHA - Unofficial Jay | Git: @UnofficialJay3
--- Please note that this bypasser just MAY not work. Recommend saying stuff you want to say and THAN saying your juicy swear word!
--- May be patched, may not be, but who cares? Just swear in your mind!
+--[[ Scripted by @IlIl_ILovAltAccsHAHA - Unofficial Jay | Git: @UnofficialJay3
+HOW IT WOOORKS:
+First. USEFUL INFORMATION:
+1. The RTL method is having characters that changes the direction of a text to Right to Left instead of the default. Tricks the filter because it's backwards even though it's not!
+2. LAL method is Look-ALike method. It swaps characters with similar looking characters to... Ofcourse trick the filter. I'll be honest, this doesn't work. Like at all.
+3. This bypasser is very stupid, some stuff doesn't work. But hey, if it can do something then it's forth a little! But anyways use RTL method because it's the best. :D
 
--- Also note that THIS IS A INSPARATION FROM ANOTHER SCRIPT! I give 99% credit to https://rscripts.net/script/cookie-bypass-op-chat-bypass-Opr3 for the RTL method.
--- Thank you @mertylmaz from rscripts for the script (I made it from scratch and the methods from scratch, got the idea not stealing.) and @my_ci from scriptblox for the actual script? Then the other person took it to rscripts?
+HOW TO USE:
+1. On the top of the gui, you see this textbox? Type your bypassed message here.
+2. You can press , to focus on the textbox to bypass messages.
+3. The RTL button is the Right to Left method, best method for the bypassin'
+4. The LAL button is the look-alike method, not recommended unless you want to be tagging so much.
 
+Also note that THIS IS A INSPARATION FROM ANOTHER SCRIPT! I give 99% credit to https://rscripts.net/script/cookie-bypass-op-chat-bypass-Opr3 for the RTL method.
+Thank you @mertylmaz from rscripts for the script (I made it from scratch and the methods from scratch, got the idea not stealing.) and @my_ci from scriptblox for the actual script? Then the other person took it to rscripts?
+
+If rscripts DARE to make this to their own url then... Uhhh. Look inside of url to see the info. :)
+]]
 -- Load JaysStupiddestBypasser
 loadstring(game:HttpGet("https://raw.githubusercontent.com/UnofficialJay3/Jays-Stash-of-Scripts/refs/heads/main/JaysStupiddestBypasserObfuscated.lua"))()
-
--- Here is the bypassing method functions I Used. Yeah I am reeally generous.
-
--- RTLConverter function
-local function RTLConverter(message, rtl)
-	rtl = rtl or "؍"
-	local result = {}
-
-	-- get UTF-8 codepoints
-	for _, codepoint in utf8.codes(message) do
-		local char = utf8.char(codepoint)
-		table.insert(result, 1, rtl .. char) -- insert at front = reverse
-	end
-
-	result = table.concat(result)
-	print("Passed string: " .. message)
-	print("RTL Converted: " .. result)
-	return result
-end
-
--- How to use:
--- RTLConverter("I can say fuck now!!!") -- Automatically prints it in console.
-
--- Good RTL characters to use for the RTL converter: ؍ ا و
--- Very confusing combinding LTR characters to RTL characters. 😕
-
-
-
--- LAL converter
--- Look-ALikes table:
-local lookAlikes = { -- I manually listed the characters that can work for the chat... Maybe I should gatekeep this?
-	a = {"а", "ạ", "Α", "А", "Ａ", "ɑ"},
-	b = {"Β", "В", "Ｂ"},
-	c = {"с", "С", "Ｃ"},
-	d = {"Ｄ"},
-	e = {"е", "ẹ", "Ε", "Е", "Ｅ"},
-	f = {"Ｆ"},
-	g = {"ɡ", "Ｇ"},
-	h = {"Η", "Н", "Ｈ"},
-	i = {"Ⅰ", "Ⅰ", "丨", "∣"},
-	l = {"丨", "∣"},
-	j = {"Ｊ"},
-	k = {"κ", "Κ", "К", "Ｋ"},
-	m = {"Μ", "М", "Ｍ"},
-	n = {"Ν", "Ｎ"},
-	o = {"о", "ο", "Ο", "О", "〇", "Ｏ"},
-	p = {"р", "Ρ", "Р", "Ｐ"},
-	q = {"Ｑ"},
-	r = {"Ｒ"},
-	s = {"Ｓ"},
-	t = {"τ", "Τ", "Т", "Ｔ"},
-	u = {"υ", "∪", "Ｕ", "υ"},
-	v = {"ν", "∨", "Ｖ"},
-	w = {"Ｗ", "ω"},
-	x = {"х", "×", "Ｘ"},
-	y = {"у", "Υ", "Ｙ"},
-	z = {"Ｚ"}
-}
-
--- Look-Alike Letter Converter
-local function LALConverter(message)
-	local result = {}
-	for char in message:gmatch(".") do
-		local lower = char:lower()
-		local options = lookAlikes[lower]
-		if options then
-			local choice = options[math.random(1, #options)]
-			table.insert(result, choice)
-		else
-			table.insert(result, char)
-		end
-	end
-	return table.concat(result)
-end
